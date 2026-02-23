b8ce18ffd0aebd01a1a4c28e6d86555229f03671
rubensbmelo <rubensbmelo@hotmail.com>
2026-02-22 21:07:51 -0300

    Peladeiros de Futevôlei website structure

---
+++ dados_jogos.js
@@ -0,0 +1,39 @@
+const bancoDeDadosJogos = [
+    {
+        data: "22/02/2026",
+        atual: true,
+        jogos: [
+            { t1: "Julio / Anderson", p: "5 x 15", t2: "Coxinha / Tarciso" },
+            { t1: "Joao / Paulo", p: "15 x 8", t2: "Alex / Rubinho" },
+            { t1: "Coxinha / Tarciso", p: "9 x 15", t2: "Rafael / Anderson" },
+            { t1: "Joao / Paulo", p: "7 x 15", t2: "Alex / Julio" },
+            { t1: "Rafael / Anderson", p: "9 x 15", t2: "Rubinho / Coxinha" },
+            { t1: "Alex / Julio", p: "10 x 15", t2: "Tarciso / Paulo" },
+            { t1: "Rubinho / Coxinha", p: "13 x 15", t2: "Joao / Rafael" },
+            { t1: "Tarciso / Paulo", p: "13 x 15", t2: "Julio / Anderson" },
+            { t1: "Joao / Rafael", p: "19 x 21", t2: "Rubinho / Alex" },
+            { t1: "Julio / Anderson", p: "13 x 15", t2: "Paulo / Coxinha" },
+            { t1: "Rubinho / Alex", p: "9 x 15", t2: "Tarciso / Joao" },
+            { t1: "Paulo / Coxinha", p: "12 x 15", t2: "Rafael / Julio" },
+            { t1: "Tarciso / Joao", p: "15 x 13", t2: "Alex / Anderson" },
+            { t1: "Rafael / Julio", p: "18 x 10", t2: "Paulo / Rubinho" }
+        ]
+    },
+    {
+        data: "15/02/2026",
+        atual: false,
+        jogos: [
+            { t1: "Paulo / Julio", p: "15 x 11", t2: "Tarciso / Capoeira" },
+            { t1: "Julio / Álvaro", p: "11 x 15", t2: "Tarciso / Diniz" },
+            { t1: "Tarciso / Diniz", p: "15 x 13", t2: "João / Fabinho" },
+            { t1: "Tarciso / Diniz", p: "15 x 8", t2: "Paulinho / Capoeira" },
+            { t1: "João / Fabinho", p: "15 x 13", t2: "Julio / Álvaro" },
+            { t1: "João / Fabinho", p: "6 x 15", t2: "Tarciso / Diniz" },
+            { t1: "Júlio / Capoeira", p: "21 x 19", t2: "Tarciso / Diniz" },
+            { t1: "Paulo / Ricardo", p: "16 x 18", t2: "Julio / Capoeira" },
+            { t1: "Julio / Thiago", p: "15 x 9", t2: "João Coxinha" },
+            { t1: "Paulinho / Coxinha", p: "15 x 9", t2: "Tarciso / Dom" },
+            { t1: "Paulo / Diniz", p: "15 x 7", t2: "Tarciso / Capoeira" }
+        ]
+    }
+];
---
+++ dados_ranking.js
@@ -0,0 +1,15 @@
+// dados_ranking.js
+const bancoDeDadosRanking = [
+    { nome: "Tarciso", total: 27, mes: 13 },
+    { nome: "Rafael", total: 26, mes: 11 },
+    { nome: "Márcio", total: 24, mes: 10 },
+    { nome: "Júlio", total: 20, mes: 13 },
+    { nome: "Rubens", total: 16, mes: 10 },
+    { nome: "Paulo", total: 15, mes: 13 },
+    { nome: "Diniz", total: 13, mes: 6 },
+    { nome: "Alex", total: 11, mes: 7 },
+    { nome: "Anderson", total: 9, mes: 8 },
+    { nome: "Thiago", total: 7, mes: 7 },
+    { nome: "Augusto", total: 6, mes: 3 },
+    { nome: "João", total: 5, mes: 5 }
+];
