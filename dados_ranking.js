4326cd4a7491e20f9f8c7332917bec2499fbc349
rubensbmelo <rubensbmelo@hotmail.com>
2026-04-27 16:46:19 -0300

    jgoos dia 26/04/2026 atualizados

--- dados_ranking.js
+++ dados_ranking.js
@@ -1,16 +1,16 @@
 const bancoDeDadosRanking = [
-    { nome: "Rafael",    total: 50, mes: 7  },
-    { nome: "Tarciso",   total: 55, mes: 14 },
-    { nome: "Júlio",     total: 41, mes: 4  },
-    { nome: "Márcio",    total: 38, mes: 0  },
-    { nome: "Rubens",    total: 42, mes: 7  },
-    { nome: "Paulo",     total: 42, mes: 16 },
-    { nome: "Fabio",     total: 22, mes: 10 },
-    { nome: "Diniz",     total: 33, mes: 7  },
-    { nome: "Alex",      total: 26, mes: 4  },
-    { nome: "Anderson",  total: 20, mes: 5  },
-    { nome: "João",      total: 22, mes: 11 },
-    { nome: "Thiago",    total: 14, mes: 0  },
+    { nome: "Rafael",    total: 56, mes: 13 },  // 7 + 6
+    { nome: "Tarciso",   total: 58, mes: 17 },  // 14 + 3
+    { nome: "Júlio",     total: 47, mes: 10 },  // 4 + 6
+    { nome: "Márcio",    total: 38, mes: 0  },  // não jogou
+    { nome: "Rubens",    total: 44, mes: 9  },  // 7 + 2
+    { nome: "Paulo",     total: 44, mes: 18 },  // 16 + 2
+    { nome: "Fabio",     total: 22, mes: 10 },  // sem alteração
+    { nome: "Diniz",     total: 37, mes: 11 },  // 33+4 / 7+4
+    { nome: "Alex",      total: 26, mes: 4  },  // sem alteração
+    { nome: "Anderson",  total: 22, mes: 7  },  // 5 + 2
+    { nome: "João",      total: 22, mes: 11 },  // sem alteração
+    { nome: "Thiago",    total: 14, mes: 0  },  // sem alteração
 ];
 
 bancoDeDadosRanking.sort((a, b) => b.total - a.total);
